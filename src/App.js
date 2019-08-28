ce34ba830c34ce050212f22b8458bd66c45bed83
wyx8267 <369264448@qq.com>
2019-08-28 10:49:37 +0800

    v1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { connect } from 'react-redux'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <span id="value">n={this.props.n}</span>
+          <div>
+          <button id="add1" onClick={() => this.props.add1()}>+1</button>
+        </div>
+      </div>
+    )
+  }
 }
 
-export default App;
+function mapStateToProps(state) {
+  return {
+    n: state.n
+  }
+}
+function mapDispatchToProps(dispatch) {
+  return {
+    add1: () => dispatch({ type: 'add', payload: 1 })
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
